fd709ec219d6d778e365c5d8809a8fab4aa5a500
Jesus Enrique Contreras Melendez <jecontre@car10s2.42barcelona.com>
2023-02-10 11:36:11 +0100

    This is an automatic message from AutoGit. This commit was submitted automatically

--- ft_next.c
+++ ft_next.c
@@ -6,11 +6,11 @@
 /*   By: jecontre <jecontre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 16:52:55 by jecontre          #+#    #+#             */
-/*   Updated: 2023/02/09 17:22:15 by jecontre         ###   ########.fr       */
+/*   Updated: 2023/02/10 11:32:59 by jecontre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "get_next_line.h"
 
 char	*ft_next(char *str)
 {
@@ -18,24 +18,43 @@ char	*ft_next(char *str)
 	int		len2;
 	char	*next;
 
-	while (str[len1] != '\n')
+	len1 = 0;
+	while (str[len1] != '\n' && str[len1])
 		len1++;
 	if (str[len1] == '\n')
 		len1++;
 	len2 = len1;
-	while (str[len2] != '\n')
+	while (str[len2] != '\n' && str[len2])
 		len2++;
-	while (str[len2] == '\n')
+	if (str[len2] == '\n')
 		len2++;
 	next = malloc(len2 * sizeof(char));
 	if (!next)
 		return (NULL);
 	len2 = 0;
-	while (str[len1] != '\n')
+	while (str[len1] != '\n' && str[len1])
 	{
 		next[len2] = str[len1];
 		len1++;
 		len2++;
 	}
+	next[len2] = '\0';
 	return (next);
+}
+
+int	main()
+{
+	char	*line;
+	char	*next;
+	char	*str;
+
+	str = "Hola que tal?\n todo bien?\n llll";
+
+	line = ft_line(str);
+	next = ft_next(str);
+	printf("%s\n", line);
+	printf("%s\n", next);
+	free(line);
+	free(next);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,38 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_line.c                                          :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jecontre <jecontre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 16:34:17 by jecontre          #+#    #+#             */
-/*   Updated: 2023/02/10 11:27:47 by jecontre         ###   ########.fr       */
+/*   Created: 2023/02/10 10:50:33 by jecontre          #+#    #+#             */
+/*   Updated: 2023/02/10 11:05:40 by jecontre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-char	*ft_line(char *str)
-{
-	char	*line;
-	int		i;
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	i = 0;
-	while (str[i] != '\n' && str[i])
-		i++;
-	if (str[i] == '\n')
-		i++;
-	line = malloc(i + 1 * sizeof(char));
-	if (!line)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\n' && str[i])
-	{
-		line[i] = str[i];
-		i++;
-	}
-	if (str[i] == '\n')
-		line[i] = '\n';
-	line[i] = '\0';
-	return (line);
-}
+char	*ft_line(char *str);
+char	*ft_next(char *str);
+
+#endif
